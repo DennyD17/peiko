0ad5a21b9ab7cf5ca7d441a271168580e491eab2
whereismysoft <kotovyuriy27@gmail.com>
2018-01-10 17:50:06 +0300

    Updated home page

--- base/static/js/home.jsx
+++ base/static/js/home.jsx
@@ -22,12 +22,16 @@ export default class Home extends React.Component{
 	}
 
 	render() {
+		const imgSize = {
+			width : '150px'
+		}
 		console.log(this.state.topProducts)
 		return(
 				<div className='products'>
 					{this.state.topProducts.map((item, index) => (
 						<div className='products__item' key={index}>
-							<div>{item.name}</div> 
+							<div>{item.name}</div>
+							<div><img style={imgSize} src={item.img_url}/></div>
 							<div>{item.description}</div>
 						</div>
 					))}
