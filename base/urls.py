3d7e9a0abbbc5826294fedd040174512fe642c8e
Denis Avramenko <Denis.avramenko1705@gmail.com>
2018-01-10 12:08:48 +0300

    Contacts page text model, about page text model, Contacts page api, about page api, fix products api, new urls

--- base/urls.py
+++ base/urls.py
@@ -1,14 +1,18 @@
-from django.urls import include, re_path
+from django.urls import re_path
 from base.views import *
 from rest_framework.routers import DefaultRouter
 
 
-urlpatterns = [
-    re_path(r"^$", IndexView.as_view(), name='index'),
-    re_path(r'^api/top-products$', TopSixProductsView.as_view()),
-    ]
-
 router = DefaultRouter()
 router.register(r'api/products', ProductViewSet)
+router.register(r'api/about', AboutPageTextViewSet)
+router.register(r'api/contacts', ContactsPageTextViewSet)
+
+urlpatterns = [
+    re_path(r"^$", IndexView.as_view(), name='index'),
+    re_path(r'^api/$', router.get_api_root_view()),
+]
+
 
 urlpatterns += router.urls
+
